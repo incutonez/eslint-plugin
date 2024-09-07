fcc3d599a90cbeeece76fd496a225ddc9e03fc04
Jef <incutonez@hotmail.com>
2024-09-07 15:20:44 -0400

    - Upgrading deps

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import arrayBracketNewline from "./rules/array-bracket-newline";
 import arrayElementNewline from "./rules/array-element-newline";
 
-// TODOJEF: Should migrate to flat config https://eslint.org/docs/latest/extend/plugins
-export = {
+export default {
+    meta: {
+        name: '@incutonez/eslint-plugin'
+    },
     rules: {
         'array-bracket-newline': arrayBracketNewline,
         'array-element-newline': arrayElementNewline
