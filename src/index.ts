f24442b8dae9bac642f0ccb9a67396ee5d9aebae
Jef <incutonez@hotmail.com>
2024-09-07 15:30:16 -0400

    - Fixing build issues

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import arrayBracketNewline from "./rules/array-bracket-newline";
 import arrayElementNewline from "./rules/array-element-newline";
 
-export default {
+export = {
     meta: {
         name: '@incutonez/eslint-plugin'
     },
